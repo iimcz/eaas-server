3042e07c156f4170062b0550231df8c656d6dadd
Rafael Gieschke <rafael@gieschke.de>
2023-06-14 11:25:56 +0200

    build(deploy-ssh): also accept EaaS instance URL as argument

--- deploy-ssh.sh
+++ deploy-ssh.sh
@@ -9,10 +9,15 @@ fi
 
 cd -- "$(dirname -- "$(realpath -- "$0" || printf %s "$0")")"
 
-set -x
 remote="$1"
+remote="${remote#http*://}"
+remote="${remote%%/*}"
+case "$remote" in *@*) ;; *) remote="ubuntu@$remote" ;; esac
 
+set -x
+remote="$remote"
 rsync -zv --progress --rsync-path="sudo rsync" -- src/ear/target/eaas-server.ear "$remote:/eaas-home/deployments/"
 ssh -- "$remote" sudo systemctl restart eaas
+set +x
 
 printf '\nssh %s\nhttps://%s\n' "$remote" "${remote##*@}"
